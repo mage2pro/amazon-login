089c46c2afbaa4ba0a0a21063564e7289249183a
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-14 02:50:01 +0300

    no message

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -1,5 +1,7 @@
 <?php
 namespace Dfe\AmazonLogin\Setup;
+// 2016-06-05
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
 class UpgradeData extends \Df\Sso\Upgrade\Data {
 	/**
 	 * 2016-06-05
@@ -8,5 +10,5 @@ class UpgradeData extends \Df\Sso\Upgrade\Data {
 	 * @used-by \Df\Sso\Upgrade\Data::attribute()
 	 * @return string
 	 */
-	protected function labelPrefix() {return 'Amazon';}
+	final protected function labelPrefix() {return 'Amazon';}
 }
